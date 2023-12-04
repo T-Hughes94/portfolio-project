3493353fe75845e6bab5ee95797ee2290bca11e5
Terence Hughes <Terencehughes450@gmail.com>
2023-12-03 19:43:41 -0500

    added profile image

--- portfolio/Components/Bio.jsx
+++ portfolio/Components/Bio.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Bio() {
   return (
     <section>
@@ -8,6 +9,7 @@ function Bio() {
       Hello! I am Terence Hughes, a passionate Software Engineer with a focus on Web Development and using A.I
       to develop systems and applications that solve real world problems.
     </p>
+    <img src={'/web-(1).jpg'} alt="Terence Hughes" />
   </section>
   )
 }
